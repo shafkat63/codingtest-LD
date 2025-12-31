8a5d5dfd66793ccfbae9e8929586979966c5f440
Muhtasir Shafkat <msm.shafkat99@gmail.com>
2025-12-31 07:35:29 +0000

    Fix initial Laravel project setup and configuration errors

--- routes/auth.php
+++ routes/auth.php
@@ -35,11 +35,13 @@ Route::middleware('guest')->group(function () {
         ->name('password.store');
 });
 
+
 Route::middleware('auth')->group(function () {
-    Route::get('verify-email', EmailVerificationPromptController::class)
+
+    Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
         ->name('verification.notice');
 
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+    Route::get('/email/verify/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
         ->middleware(['signed', 'throttle:6,1'])
         ->name('verification.verify');
 
@@ -52,7 +54,8 @@ Route::middleware('auth')->group(function () {
 
     Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
-    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+    Route::put('password', [PasswordController::class, 'update'])
+        ->name('password.update');
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
